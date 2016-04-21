a2c3a23cb11676691dc9539f38f4c6960744b408
godsleftnut <mike@thinkitfirst.com>
2016-04-21 03:30:21 +0000

    user control panel, single post page, and edited roles

--- wp-content/themes/tif-theme-master/single.php
+++ wp-content/themes/tif-theme-master/single.php
@@ -2,11 +2,20 @@
 
 <?php include_once('inc/banner-page.php'); ?>
 
-<div class="full-width">
+<div class="full-width" id="singlePage">
 	<div class="container-fluid">
-		<div class="col-xs-24">
+		<div class="inner col-xs-24">
 			
 			<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+			
+			<?php the_title(); ?>
+			
+			<ul>
+				<li class="date"><?php the_date('m-d-Y'); ?><span> / </span></li>
+				<li class="author"><?php the_author(); ?></li>
+			</ul>
+			
+			<div class="single-rule"></div>
 			
 			<?php the_content(); ?>
 			
